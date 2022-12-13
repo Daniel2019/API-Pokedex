d97ead4144d8afd6037a79953218c2c3f90d1496
Daniel Soares <daniel_dandico@outlook.com>
2022-12-12 22:53:02 -0300

    Feat: Commit Inicial

---
+++ apipokedex/src/main/java/com/mycompany/apipokedex/Apipokedex.java
@@ -0,0 +1,12 @@
+package com.mycompany.apipokedex;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Apipokedex {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! Welcome a API Pokédex!");
+    }
+}
